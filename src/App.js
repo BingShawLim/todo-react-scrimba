c02fe1f6ccfb27f5481398934caf046e65d4acda
BingShawLim <shaw.a.muvee@gmail.com>
2020-05-07 13:32:18 -0500

    add CSS styles.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import TodoItem from "./components/TodoItem"
 
 function App() {
   return (
-    <div className="App">
-      <h1>HAHAHA!</h1>
+    <div className="todo-list">
+      <TodoItem />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/components/TodoItem.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+export class TodoItem extends Component {
+    render() {
+        return (
+            <div className="todo-item">
+            <input type="checkbox"/>
+                <p>hello!!!</p>
+            </div>
+        )
+    }
+}
+
+export default TodoItem
+
